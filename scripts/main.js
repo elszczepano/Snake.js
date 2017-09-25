6ac9cc7aa8acb34af949392d0889e96c287aff2e
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2017-09-25 22:13:35 +0200

    Creating snake, creating snake food, move effect

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,46 @@
-const playground = document.querySelector('canvas');
-const ctx = playground.getContext('2d');
+
+  const playground = document.querySelector('canvas');
+  const ctx = playground.getContext('2d');
 
 playground.width = 500;
 playground.height = 500;
 
-const snakeMeasurement = 15;
+const snakeMeasurement = 20;
 const snakeColor = "#ffff33";
+const foodColor = "#33ff00";
+const snakeSpeed = 1.2;
+let foodX = Math.random()*480;
+let foodY = Math.random()*480;
 
-function drawPlayground() {
-      ctx.fillRect(0,0,playground.width,playground.height);
-}
+let foodCollected = 3;
+
+let snakeX = playground.width / 2 - snakeMeasurement / 2
+let snakeY = playground.height / 2 - snakeMeasurement / 2
+
 function drawSnake() {
       ctx.fillStyle = snakeColor;
-      ctx.fillRect(playground.width/2-10,playground.height/2-10,snakeMeasurement,snakeMeasurement);
+      for(let i=0; i<foodCollected; i++)
+      {
+        ctx.fillRect(snakeX,snakeY+(i*25),snakeMeasurement,snakeMeasurement);
+        snakeY -= snakeSpeed;
+      }
 }
-drawPlayground();
-drawSnake();
+function drawSnakeFood()
+{
+  ctx.fillStyle = foodColor;
+  ctx.fillRect(foodX,foodY,snakeMeasurement,snakeMeasurement);
+}
+function drawPlayground() {
+    ctx.fillStyle = "#000";
+      ctx.fillRect(0,0,playground.width,playground.height);
+}
+function gameInit()
+{
+  drawPlayground();
+  drawSnake();
+  drawSnakeFood();
+}
+setInterval(gameInit,20);
+
+
+drawSnakeFood();
