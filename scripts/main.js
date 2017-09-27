cb363d9bbeb566e1ae4197b9ead2fc7e167889e1
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2017-09-27 19:02:34 +0200

    Create grid. Now the snake is moving correctly. Eating food with bugs. No detect collision with borders.

--- scripts/main.js
+++ scripts/main.js
@@ -5,19 +5,24 @@ document.addEventListener("keydown",moveSnake);
 playground.width = 500;
 playground.height = 500;
 
-const snakeMeasurement = 20;
+const gridSize = 20;
 const snakeColor = "#ffff33";
 const foodColor = "#33ff00";
+let tileCount = playground.width/gridSize;
 let velocityX = 0;
 let velocityY = 0;
-let foodX = Math.random()*480;
-let foodY = Math.random()*480;
+let trail = [];
+let snakeTail = 5;
+let foodX = 20;
+let foodY = 20;
 
-let foodCollected = 3;
+let snakeX = playground.width / 2 - gridSize / 2;
+let snakeY = playground.height / 2 - gridSize / 2;
 
-let snakeX = playground.width / 2 - snakeMeasurement / 2
-let snakeY = playground.height / 2 - snakeMeasurement / 2
-
+function drawPlayground() {
+    ctx.fillStyle = "#000";
+      ctx.fillRect(0,0,playground.width,playground.height);
+}
 function moveSnake(ev) {
     switch (ev.keyCode) {
       case 37:
@@ -42,45 +47,59 @@ function moveSnake(ev) {
         break;
     }
 }
-function drawSnake(snakeBlocks) {
-        ctx.fillStyle = snakeColor;
-        ctx.fillRect(snakeX,snakeY+(snakeBlocks*25),snakeMeasurement,snakeMeasurement);
-        snakeX += velocityX;
-        snakeY += velocityY;
-}
 
 function drawSnakeFood() {
-  ctx.fillStyle = foodColor;
-  ctx.fillRect(foodX,foodY,snakeMeasurement,snakeMeasurement);
-}
+        if(snakeX==foodX && snakeY==foodY) {
+          snakeTail++;
+          foodX = Math.floor(Math.random()*tileCount);
+          foodY = Math.floor(Math.random()*tileCount);
+          }
+          ctx.fillStyle = foodColor;
+          ctx.fillRect(foodX * gridSize,foodX * gridSize,gridSize-2,gridSize-2);
+          }
+function drawSnake() {
 
-function drawPlayground() {
-    ctx.fillStyle = "#000";
-      ctx.fillRect(0,0,playground.width,playground.height);
-}
-
-function detectCollision() {
-  if(snakeY <= 0 || snakeY >= 500 || snakeX <= 0 || snakeX >= 500 ) {
-    for(let i = 0; i < foodCollected; i++)
-    {
-      ctx.fillStyle = snakeColor;
-      ctx.fillRect(snakeX,snakeY+(i*25),snakeMeasurement,snakeMeasurement);
-    }
-    //for later use... disabled in production.
-    //alert("Game over! Your score: "+(foodCollected-3)+" points. Wanna play again?");
-    //location.reload();
-  }
-  else {
-    for(let i = 0; i < foodCollected; i++)
-    {
-      drawSnake(i);
-    }
-  }
+         snakeX += velocityX;
+         snakeY += velocityY;
+        if(snakeX < 0) {
+          snakeX = tileCount-1;
+        }
+        if(snakeX > tileCount-1) {
+          snakeX = 0;
+        }
+        if(snakeY < 0) {
+          snakeY = tileCount-1;
+        }
+        if(snakeY > tileCount-1) {
+          snakeY = 0;
+        }
+        drawPlayground();
+        ctx.fillStyle = snakeColor;
+        for(let i = 0; i < trail.length; i++) {
+            ctx.fillRect(trail[i].x * gridSize,trail[i].y * gridSize,gridSize-2,gridSize-2);
+            if(trail[i].x == snakeX && trail[i].y == snakeY) {
+                snakeTail = 5;
+            }
+        }
+        trail.push({x:snakeX,y:snakeY});
+        while(trail.length>snakeTail) {
+        trail.shift();
+        }
 }
 
 function gameInit() {
-  drawPlayground();
-  detectCollision();
+  drawSnake();
   drawSnakeFood();
 }
-setInterval(gameInit,1000/60);
+
+// function detectCollision() {
+//   if(!(snakeY <= 0 || snakeY >= playground.width || snakeX <= 0 || snakeX >= playground.height )) {
+//
+//   }
+//   else {
+//     //for later use... disabled in production.
+//     //alert("Game over! Your score: "+(snakeTail-5)+" points. Wanna play again?");
+//     //location.reload();
+//   }
+// }
+setInterval(gameInit,100);
